6b239aaee88aedfb52fa5a5e3f3056f3dd5a87cd
ricardobizerra <ricardo.bizerra19@gmail.com>
2024-02-10 13:44:29 -0300

    feat: get poll and its options

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,9 +1,11 @@
 import fastify from 'fastify';
 import { createPoll } from './routes/create-poll';
+import { getPoll } from './routes/get-poll';
 
 const app = fastify();
 
 app.register(createPoll);
+app.register(getPoll);
 
 app.get('/health', () => {
     return 'ok!';
