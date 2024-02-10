317cd6ad1375f5c93ff37e246efcd47ac17c242f
ricardobizerra <ricardo.bizerra19@gmail.com>
2024-02-10 16:10:25 -0300

    feat: add vote on poll

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,11 +1,19 @@
 import fastify from 'fastify';
 import { createPoll } from './routes/create-poll';
 import { getPoll } from './routes/get-poll';
+import { voteOnPoll } from './routes/vote-on-poll';
+import fastifyCookie from '@fastify/cookie';
 
 const app = fastify();
 
+app.register(fastifyCookie, {
+    secret: process.env.FASTIFY_COOKIE_SECRET,
+    hook: 'onRequest',
+});
+
 app.register(createPoll);
 app.register(getPoll);
+app.register(voteOnPoll);
 
 app.get('/health', () => {
     return 'ok!';
